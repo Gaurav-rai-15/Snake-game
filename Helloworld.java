39a52cf6f47e10648af2b608bf23d056f548435f
Gaurav-rai-15 <117948389+Gaurav-rai-15@users.noreply.github.com>
2023-01-10 04:52:02 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Hello World");
+		int n=sc.nextInt();
+		System.out.print(n);
+
+	}
+
+}
